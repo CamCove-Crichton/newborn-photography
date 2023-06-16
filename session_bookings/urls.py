2d61e6c47bf814f6344f10ad512f0fd61377ec69
CamCove-Crichton <>
2023-06-16 06:14:58 +0000

    Update bookings.html to link to booking_details.html and wire up the booking detail view

--- session_bookings/urls.py
+++ session_bookings/urls.py
@@ -2,6 +2,8 @@
 from . import views
 from django.urls import path
 
+# Assitance from code institutes I think therefore I blog walkthrough tutorials
 urlpatterns = [
     path('', views.BookingList.as_view(), name='bookings'),
+    path('<slug:slug>', views.BookingDetail.as_view(), name='booking_detail'),
 ]
--- session_bookings/views.py
+++ session_bookings/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # Assitance from code institutes I think therefore I blog walkthrough tutorials
-from django.views import generic
+from django.views import generic, View
 from .models import Booking
+
+# Assitance from code institutes I think therefore I blog walkthrough tutorials
 
 
 class BookingList(generic.ListView):
@@ -9,3 +11,19 @@ class BookingList(generic.ListView):
     queryset = Booking.objects.order_by('-booking_date')
     template_name = 'bookings.html'
     paginate_by = 6
+
+
+# Assitance from code institutes I think therefore I blog walkthrough tutorials
+class BookingDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Booking.objects
+        booking = get_object_or_404(queryset, slug=slug)
+
+        return render(
+            request,
+            "booking_detail.html",
+            {
+                "booking": booking
+            },
+        )
