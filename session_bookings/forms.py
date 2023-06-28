a2e78ecb144a69616a0aeb6148c80483734c3232
CamCove-Crichton <>
2023-06-28 06:20:41 +0000

    Create new form class for the Todo form and add the todo section in the booking_details.html template for superusers only

--- session_bookings/forms.py
+++ session_bookings/forms.py
@@ -1,5 +1,5 @@
 # Assitance from code institutes I think therefore I blog walkthrough tutorials
-from .models import Booking
+from .models import Booking, Todo
 from django.contrib.auth.models import User
 from django import forms
 
@@ -11,7 +11,8 @@ class BookingForm(forms.ModelForm):
     the html template
     """
     # Assitance from ChatGpt on how to change the accepted date format input in the form
-    booking_date = forms.DateField(input_formats=['%d-%m-%y', '%d-%m-%Y'], widget=forms.DateInput(format='%d-%m-%y'),)
+    booking_date = forms.DateField(
+        input_formats=['%d-%m-%y', '%d-%m-%Y'], widget=forms.DateInput(format='%d-%m-%y'),)
     babys_due_date = forms.DateField(
         input_formats=['%d-%m-%y', '%d-%m-%Y'], widget=forms.DateInput(format='%d-%m-%y'),)
     booking_time = forms.TimeField(
@@ -23,3 +24,18 @@ class BookingForm(forms.ModelForm):
                   'babys_due_date', 'babys_name', 'babys_gender',
                   'location', 'special_requests', 'how_you_found_me',
                   'consent', 'featured_image',)
+
+
+class TodoForm(forms.ModelForm):
+    """
+    The Todo form allows the admin to fill in details about the Todo item,
+    utilising crispy forms in the html template
+    """
+    due_date = forms.DateField(
+        input_formats=['%d-%m-%y', '%d-%m-%Y'], widget=forms.DateInput(format='%d-%m-%y'),)
+    due_time = forms.TimeField(
+        input_formats=['%H:%M'], widget=forms.TimeInput(format='%H:%M'),)
+
+    class Meta:
+        model = Todo
+        fields = ('title', 'details', 'due_date', 'due_time', 'completed',)
